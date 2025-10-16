72ac990ab4de348a08e843afeda7f129ec5ea459
rizalramadani <rizalrama145@gmail.com>
2025-10-16 12:42:53 +0700

    Add files via upload

---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloFunctional.java
@@ -0,0 +1,11 @@
+// HelloFunctional
+import java.util.function.BiConsumer;
+
+public class HelloFunctional {
+    public static void main(String[] args) {
+        BiConsumer<String, String> introduce =
+            (nim, name) -> System.out.println("Hello World, I am " + name + " - " + nim);
+
+        introduce.accept("240202883", "Rizal Ramadani");
+    }
+}
